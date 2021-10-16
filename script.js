aa06557a30547947de676d5e15d51bc73d27402f
Alejandro <aariasmartijena@gmail.com>
2021-10-16 11:05:55 -0400

    Formatted with more legible code and meaningful comments

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function game() {
     let computerWins = 0;
 
     while (playerWins < 5 && computerWins < 5) { //Keep playing until someone gets to 5 points.
-        let playerSelection = prompt('Enter your play: '); //Prompts the user to insert his play.
-        let computerSelection = computerPlay(); //Gets the computer's play.
+        let playerSelection = prompt('Enter your play: ');
+        let computerSelection = computerPlay();
         wins = playRound(playerSelection,computerSelection);
-        if (wins[0] === 1) { //If the player beat the computer on this round, give them 1 point.
+        if (wins[0] === 1) {
             playerWins++;
-        } else if (wins[1] === 1) { //If the computer beat the player on this round, give it a point.
+        } else if (wins[1] === 1) {
             computerWins++;
         }
-        console.log(`Player has ${playerWins} points. Computer has ${computerWins} points.`); //Tells the player the score.
+        console.log(`Player has ${playerWins} points. Computer has ${computerWins} points.`);
     }
 
-    if (playerWins > computerWins) { //Let the player know who got to 5 points first.
+    if (playerWins > computerWins) {
         console.log('The player wins this time!'); 
     } else {
         console.log('The computer wins this time!');
@@ -26,7 +26,7 @@ function computerPlay() {
     let choice = Math.round(Math.random() * 2); //Randomize the computer's play using a whole number from 0 to 2.
     let play = "";
 
-    switch(choice) { //Assigning a play to each number value.
+    switch(choice) {
         case 0: 
             play = "rock";
             break;
@@ -40,43 +40,43 @@ function computerPlay() {
     return play;
 }
 
-function playRound(player,computer) {
+function playRound(playerSelection,computerSelection) {
     let wins = [0, 0]; //The round always starts with no score.
-    player = player.split('').map(x => x.toLowerCase()).join(''); //Converts any player input to lower case.
-    console.log(`Player played ${player}, computer played ${computer}.`); //Displays the player and the computer's plays for this round.
+    playerSelection = playerSelection.split('').map(x => x.toLowerCase()).join('');
+    console.log(`Player played ${playerSelection}, computer played ${computerSelection}.`);
     
-    switch (player) { //Decisions to determine who wins given the player's play.
+    switch (playerSelection) {
         case "rock":
-            if (computer === "rock") {
+            if (computerSelection === "rock") {
                 console.log('Draw!');
-            } else if (computer === "paper") {
+            } else if (computerSelection === "paper") {
                 console.log('Computer wins!');
                 wins[1] += 1;
-            } else if (computer === "scissor") {
+            } else if (computerSelection === "scissor") {
                 console.log('Player wins!');
                 wins[0] += 1;
             }
             break;
 
         case "paper":
-            if (computer === "paper") {
+            if (computerSelection === "paper") {
                 console.log('Draw!');
-            } else if (computer === "scissor") {
+            } else if (computerSelection === "scissor") {
                 console.log('Computer wins!');
                 wins[1] += 1;
-            } else if (computer === "rock") {
+            } else if (computerSelection === "rock") {
                 console.log('Player wins!');
                 wins[0] += 1;
             }
             break;
 
         case "scissor":
-            if (computer === "scissor") {
+            if (computerSelection === "scissor") {
                 console.log('Draw!');
-            } else if (computer === "rock") {
+            } else if (computerSelection === "rock") {
                 console.log('Computer wins!');
                 wins[1] += 1;
-            } else if (computer === "paper") {
+            } else if (computerSelection === "paper") {
                 console.log('Player wins!');
                 wins[0] += 1;
             }
@@ -87,5 +87,5 @@ function playRound(player,computer) {
             break;
     }
     
-    return wins; //Returns an array which represents who won the round. wins == [1, 0] if the player won, wins == [0, 1] if the computer won.
+    return wins;
 }
